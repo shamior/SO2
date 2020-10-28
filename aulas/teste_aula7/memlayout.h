d58377937eb44ec11b9bfba3009f58c8a72c4ec9
[shamior] <[shamior_@hotmail.com]>
2020-10-28 14:36:47 -0300

    teste_aula7 deletado

--- aulas/teste_aula7/memlayout.h
+++
@@ -1,4 +0,0 @@
-#define UART 0x10000000L
-#define KERNEL_START 0x80000000L
-#define MEMORY_LENGTH 128 * 1024 * 1024 // 128 MiB  - A memória que o kernel enxerga
-#define PAGE_SIZE  4096
--- aulas/teste_aula7/types.h
+++
@@ -1,7 +0,0 @@
-#ifndef TYPES_HEADER
-#define TYPES_HEADER
-typedef unsigned char uint8;
-typedef unsigned short uint16;
-typedef unsigned int uint32;
-typedef unsigned long uint64;
-#endif
